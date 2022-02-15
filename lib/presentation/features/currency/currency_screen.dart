e2f06b89fef5876e6ef1062fdd3e72272c76352b
Ogheneovo Atibaka <oatibaka@bestaftechnologies.com>
2022-02-15 19:35:56 +0100

    re-built appbar for currency screen

--- lib/presentation/features/currency/currency_screen.dart
+++ lib/presentation/features/currency/currency_screen.dart
@@ -9,41 +9,23 @@ class CurrencyScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: AppColor.bgColor,
-      child: Column(
-        children: [
-          SizedBox(
-            width: 411.w,
-            height: 21.h,
-          ),
-          Container(
-            width: 411.w,
-            height: 60.h,
-            child: Row(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(
-                      top: 17.0.sp,
-                      bottom: 31.0.sp,
-                      left: 35.0.sp,
-                    ),
-                  child: Icon(
-                    Icons.arrow_back,
-                    color: Colors.black,
-                    semanticLabel: 'Press to go to previous screen',
-                  ),
-                ),
-                SizedBox(width: 112.w),
-                Text(
-                  "Currency",
-                  style: AppTextStyle.balanceTextStyle,
-                )
-              ],
-            )
-          ),
-      ]
-       )
-      );
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColor.primaryColor,
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+       leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+      onPressed: () => Navigator.of(context).pop(),
+       ),
+        title: Text(
+            "Currency",
+            style: AppTextStyle.balanceTextStyle
+        ),
+      ),
+      body: Container(
+        color: Colors.white38,
+      ),
+    );
   }
 }
